f7506faae52f51514bfa183b5f53d1d5f7945a76
Nazar Tsebryk <daniejib@gmail.com>
2020-02-02 12:47:27 +0200

    As admin I want to have possibility to create new account #1 (Fixed issue, with access to /users using any Authority)

--- src/main/java/com/ntsebryk/timemate/configuration/WebSecurityConfig.java
+++ src/main/java/com/ntsebryk/timemate/configuration/WebSecurityConfig.java
@@ -47,9 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.csrf().disable()
 			.authorizeRequests()
-			.antMatchers("api/**").authenticated()
-			//TODO role verification is not working
-			.antMatchers("api/users").hasRole("ADMIN")
+			.antMatchers("/api/users/**").hasRole("ADMIN")
+			.antMatchers("/api/**").authenticated()
 			.anyRequest().authenticated();
 	}
 
